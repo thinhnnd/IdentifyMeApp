9de1488d4220363cf5ebf81c29fcb56c5e718c4d
thinhnnd <16521179@gm.uit.edu.vn>
2020-03-13 09:03:36 +0700

    init project, add navigation for page

---
+++ IdentifyMe.App/IdentifyMe.App/Views/Credentials/Credentials.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IdentifyMe.App.Views.Credentials
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Credentials : ContentPage
+    {
+        public Credentials()
+        {
+            InitializeComponent();
+        }
+    }
+}
